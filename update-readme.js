8952ee67cb071d6a4a01421d6e1b75b3beab589d
JuliusUlee <61336116+Julius-Ulee@users.noreply.github.com>
2024-01-03 06:50:25 +0700

    :octocat: Announcements Anime

--- update-readme.js
+++ update-readme.js
@@ -73,6 +73,9 @@ async function updateReadmeWithAnimeData() {
       readmeContent += `<td align="center"><a href="${anime.link}">Anime Information</a></td>\n`;
       readmeContent += `</tr>\n`;
       readmeContent += `<tr>\n`;
+      readmeContent += `<td colspan="2">📙 Description :</td>`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `<tr>\n`;
       readmeContent += `<td colspan="2">\n`;
       readmeContent += `<p align="center">${anime.description}</p>\n`; 
       readmeContent += `</td>\n`;
