29b5ed3d7f8870e5111bbf5cd3da9bb8fedb7757
JuliusUlee <61336116+Julius-Ulee@users.noreply.github.com>
2024-01-29 17:58:55 +0700

    🚀 Update Status Badges

--- update-readme.js
+++ update-readme.js
@@ -50,15 +50,15 @@ async function updateReadmeWithAnimeData() {
     readmeContent += `<p align="center"><img src="img/news.png" height="100"></p>`;
     readmeContent += `<p align="center">This script aims to automate the process of updating the latest anime information, so that users do not need to do it manually. This makes it easier for users to know what anime are newly released and makes it easier for them to access more information.</p>`;
     readmeContent += `<p align="center">
-    <img align="center" src="https://img.shields.io/github/contributors/Julius-Ulee/Announcements-Anime"> 
-    <img align="center" src="https://img.shields.io/github/issues/Julius-Ulee/Announcements-Anime"> 
-    <img align="center" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=shields"> 
-    <img align="center" src="https://img.shields.io/github/issues-pr/Julius-Ulee/Announcements-Anime"> 
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime/graphs/contributors"><img align="center" src="https://img.shields.io/github/contributors/Julius-Ulee/Announcements-Anime"></a> 
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime/issues"><img align="center" src="https://img.shields.io/github/issues/Julius-Ulee/Announcements-Anime"></a> 
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime/pulls"><img align="center" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=shields"></a> 
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime/pulls"><img align="center" src="https://img.shields.io/github/issues-pr/Julius-Ulee/Announcements-Anime"></a> 
     <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/LICENSE"><img align="center" src="https://img.shields.io/badge/License-MIT-yellow.svg"></a>
-    <img align="center" src="https://img.shields.io/github/commit-activity/m/Julius-Ulee/Announcements-Anime"> 
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime/commits/master/"><img align="center" src="https://img.shields.io/github/commit-activity/m/Julius-Ulee/Announcements-Anime"></a> 
     <img align="center" src="https://github.com/Julius-Ulee/github-profile-views-counter/blob/master/svg/738176371/badge.svg"> 
     <img align="center" height='20' src="https://github.com/Julius-Ulee/github-profile-views-counter/blob/master/graph/738176371/small/week.png"><br>
-    <img align="center" src="https://img.shields.io/maintenance/yes/2024"></p>`;
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime"><img align="center" src="https://img.shields.io/maintenance/yes/2024"></a></p>`;
     readmeContent += `<p align="center"><b>Display GitHub Action Badge</b> <a href="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/black.yml"><img align="center" src="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/black.yml/badge.svg"></a> <a href="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/pages.yml"><img align="center" src="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/pages.yml/badge.svg"></a></p>`;
     readmeContent += `<p align="center"><a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/README.md">Readme</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/.github/workflows/black.yml">Actions-YML</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/.gitignore">Gitignore</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/LICENSE">License</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/update-readme.js">Update-readme</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/package.json">Package</a></p>`;
 
